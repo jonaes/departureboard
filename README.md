97682496709453f76cf7ae033541c655f6c6d769
Jonaes <jonaes02@gmx.de>
2021-05-30 17:32:23 +0200

    first version

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # departureboard
-NodeMCU LCD train departure board
+**Arduino-Sketch zur Anzeige einer Bahnhofstafel auf einem 20x4-LCD-Display.**
+
+## Aktuelle Features
+
+* Anzeige der nächsten 4 Abfahrten eines Bahnhofes mit Daten der dbf-API von [https://github.com/derf](@derf) unter [https://dbf.finalrewind.org/]
+  *  Liniennr., Ziel, Minuten bis zur realen Abfahrt
+  * Anzeige von Verspätungen >3 Min durch einen Stern *
+* Zugziele werden für eine bessere Darstellung etwas gekürzt
+* Aktualisierung ca. alle 1:20 Minuten
+
+* weitere sind geplant
+
+## Voraussetzungen
+(das ist jedenfalls meine Hardware, auf der sollte der Sketch ohne Anpassungen laufen)
+* NodeMCU CH340 ESP-8266 ([https://de.aliexpress.com/item/1005001636634198.html?spm=a2g0s.9042311.0.0.5ed64c4dZIBFtu](dieses hier))
+* 20 x 4 I2C LCD-Display ([https://de.aliexpress.com/item/1005001636301479.html?spm=a2g0s.9042311.0.0.5ed64c4dZIBFtu](jenes dort))
+* SD-Card-Leser ([https://de.aliexpress.com/item/2046499166.html?spm=a2g0s.9042311.0.0.5ed64c4dZIBFtu](das da))
+  * eine SD-Karte (eine alte 2 GB-Karte, die hier noch rumflog)
+* WLAN
+
+Bibliotheken:
+* ESP8266WiFi
+* ESP8266HTTPClient
+* SPI
+* SD
+
+* Regexp von Nick Gammon  nickgammon/Regexp 
+* LiquidCrystal I2C von Frank de Brabander  johnrickman/LiquidCrystal_I2C 
+* NTPClient von Fabrice Weinberg   arduino-libraries/NTPClient 
+* ArduinoJSON von Benoit Blanchon   bblanchon/ArduinoJson 
+
+### Aufbau:
+NodeMCU -> SD
+3V -> 3V
+G -> GND
+D5 -> SCK
+D6 -> MISO
+D7 -> MOSI
+D8 -> CS
+
+NodeMCU -> LCD
+G -> GND
+VU -> VCC
+D1 -> SCL
+D2 -> SDA
+
+
+TODO: Hier noch Bilder hin
+
+## Bekannte Probleme
+* von Zeit zu Zeit läuft etwas mit dem Countdown schief (muss ich mir mal anschauen, warum)
+
+* im Allgemeinen ist der Code vermutlich unglaublich unelegant. Mein Wissen über C++ beschränkt sich auf "das ist im Prinzip so ähnlich wie Java (dassen Wissen sich wiederum auf 1 Jahr Schule beschränkt)" und Codeschnipseln aus dem Internet. Verbesserungsvorschläge willkommen.
+
+## Lizenz
+habe ich mich noch nicht mit beschäftigt. Einige der Libraries stehen unter GNU GPL, glaube ich. Bitte entsprechend beachten.
--- departureboard.ino
+++ departureboard.ino
@@ -1,1 +1,243 @@
+//--------------------------------------------------
+// https://github.com/jonaes/departureboard
+// 
+// Lizenzen der Libaries beachten!
+//
+//--------------------------------------------------
 
+#include <Regexp.h>
+#include <LiquidCrystal_I2C.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+#include <SPI.h>
+#include <SD.h>
+#include <WiFiClientSecure.h>
+
+
+LiquidCrystal_I2C disp(0x27, 20, 4);
+
+File dbf;
+
+// WiFi Parameters
+const char* ssid = "******";
+const char* password = "******";
+
+// Zeitzone
+const long utcOffsetInSeconds = 3600 * 2;
+
+
+// API definieren
+String station = "Frankfurt(Main)Hbf";  //Aktuell ohne Funktion, TODO URL aus dieser Var erzeugen
+//const char* host = "https://marudor.de/api/iris/v2/abfahrten/8004596?lookahead=15";
+const char* host = "https://dbf.finalrewind.org/Frankfurt(Main)Hbf.json?version=3&limit=8&admode=dep";
+//const char* host = "https://jsonplaceholder.typicode.com/todos/1";
+
+
+
+void setup() {
+  
+  disp.init();
+  disp.clear();
+  disp.backlight();
+  disp.setCursor(0, 0);
+  Serial.begin(115200);
+  
+  
+  connectWiFi();
+
+  SD.begin(15);
+}
+
+void loop() {
+  
+	displayLCD(dlParse());
+	delay(60000);
+}
+
+DynamicJsonDocument dlParse() {
+  String payload;
+
+  SD.remove("dbf.json");
+  dbf = SD.open("dbf.json", FILE_WRITE);
+
+  // Check WiFi Status
+  if (WiFi.status() == WL_CONNECTED) {
+    if (dbf) {
+
+      HTTPClient http;
+      WiFiClientSecure client;
+      client.setInsecure();
+      client.connect(host, 80);
+
+      Serial.println("host connected");
+      http.begin(client, host);
+
+      Serial.println("Lade url...");
+
+      int httpCode = http.GET();
+      Serial.println(httpCode);
+  
+      Serial.println("Erfolgreich geladen");
+      //disp.setCursor(0, 3); disp.print("Geladen. Parse...");
+      Serial.println("Schreibe in Textdatei...");
+      dbf.print(http.getString());
+
+      dbf.close();
+      Serial.println("Abgeschlossen.");
+      Serial.println();
+      http.end();
+    }
+  }
+
+  dbf = SD.open("dbf.json");
+  // Allocate JsonBuffer
+  // Use arduinojson.org/assistant to compute the capacity.
+  const size_t capacity = 12288;
+  DynamicJsonDocument doc(capacity);
+
+  // Parse JSON object
+  DeserializationError err = deserializeJson(doc, dbf);
+  Serial.println(err.c_str());
+
+  return doc;
+}
+
+void connectWiFi() {
+  WiFi.begin(ssid, password);
+  disp.setCursor(0, 0); disp.print("Verbinde...");
+
+  WiFiClientSecure client;
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting...");
+  }
+  //disp.print("connected");
+
+  Serial.println("Initialisierung abgeschlossen");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  disp.setCursor(0, 1); disp.print(WiFi.localIP());
+  disp.setCursor(0,2); disp.print("API von @derfnull; GNU AGPL 3");
+  disp.setCursor(0,3); disp.print("dbf.finalrewind.org");
+}
+
+void displayLCD(DynamicJsonDocument doc) { //LCD-Anzeige aufbauen
+
+  /*Serial.println(doc["departures"][0]["train"]["name"].as<char*>());
+  Serial.println(doc["departures"][0]["train"].as<char*>());
+
+  Serial.print("xx  ");
+  Serial.print(doc["departures"][0]["destination"].as<char*>());
+  Serial.print(" xx ");
+  Serial.print( doc["departures"][0]["scheduledDeparture"].as<char*>());*/
+  
+
+  disp.clear();
+  disp.backlight();
+  int j = 0;
+  int d = 0;
+  while ( d < 4) {
+    if (doc["departures"][j]["destination"].as<String>() == station) {
+      j++;
+      continue;
+    }
+
+    disp.setCursor(0, d);
+    disp.print(shortenTrain(doc["departures"][j]["train"].as<char*>()));				//TODO Überstehende Zugnummern verarbeiten
+    disp.setCursor(4, d);
+    disp.print(shortenDest(doc["departures"][j]["destination"].as<char*>(), 13));
+    disp.setCursor(17, d);
+    if ( doc["departures"][j]["delayDeparture"].as<int>() > 3) {
+      disp.print("*");
+    }
+    else {
+      disp.print(" ");
+    }
+    if ( timeToMin(doc["departures"][j]["scheduledDeparture"].as<String>()) +  doc["departures"][j]["delayDeparture"].as<int>() - currentTimeInMin() > 9) {
+      disp.print( timeToMin(doc["departures"][j]["scheduledDeparture"].as<String>()) +  doc["departures"][j]["delayDeparture"].as<int>() - currentTimeInMin());
+    }
+    else {
+      disp.print(" ");
+      disp.print( timeToMin(doc["departures"][j]["scheduledDeparture"].as<String>()) +  doc["departures"][j]["delayDeparture"].as<int>() - currentTimeInMin());
+    }
+    j++;
+    d++;
+  }
+}
+
+String shortenDest(String input, int mgl) { //Bahnhofsnamen kürzen
+  char ipt[30];
+  char res;
+
+  if (station == input) { //Legacy (bevor ich daran gedacht habe, dass API-Parameter existieren
+    input = "Zug endet";
+    return input;
+  }
+ 
+  //Umlaute für LCD umwandeln
+  input.replace("ü", "\xF5"); input.replace("ä", "\xE1"); input.replace("ß", "\xE2"); input.replace("ö", "\xEF");
+
+  input.toCharArray(ipt, 30);
+  MatchState ms (ipt);
+
+
+  ms.GlobalReplace( "%(.*%)", " ");             //Klammerzusätze entfernen
+ // ms.GlobalReplace("Bad%s", "B.");              //"Bad" zu "B" kürzen
+  ms.GlobalReplace("bahnhof", "bf");
+  ms.GlobalReplace("Bahnhof", "Bf");
+
+  input = ipt;
+  if (input.length() > mgl) {		//Länger als mögliche Anzeige?
+    int cut = input.length() - mgl;
+    Serial.println(cut);
+    Serial.println(input.length());
+    res = ms.Match(".+%s.+");		//enthält mehrere Wörter?
+    if (res) {						//vom ersten Wort die überstehenden Zeichen wegnehmen (regexp.h kann keine {}-Quantifier
+      String reg = "";
+      for (int r = 0; r < cut; r++) {
+        reg += "[a-z]";
+      }
+      reg += "%s";
+      Serial.println(reg.c_str());
+      ms.GlobalReplace(reg.c_str(), " "); //          //Erstes Wort kürzen
+
+
+    }
+    input = ipt;
+    input = input.substring(0, mgl);
+    return input;
+  }
+}
+
+String shortenTrain(String input) {
+  if (input.indexOf("ICE") >= 0) {
+    return "ICE";
+  } else if (input.indexOf("IC") >= 0) {
+    return "IC";
+  }
+  input.replace(" ", "");
+  input = input.substring(0,5);
+  return input;
+}
+
+int timeToMin(String timeStr) {
+  int hr = timeStr.substring(0, 2).toInt();
+  int hrMins = timeStr.substring(3).toInt();
+  int mins = (hr * 60) + hrMins;
+  return mins;
+}
+
+int currentTimeInMin() {
+  WiFiUDP ntpUDP;
+  NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+  timeClient.begin();
+  timeClient.update();
+  int mins = (timeClient.getHours() * 60) + timeClient.getMinutes();
+  Serial.println("akt zeit:");
+  Serial.println(mins);
+  return mins;
+}
